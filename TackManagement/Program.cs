843323620983b25e514507d72c93417eb364cc8a
Влад Подрез <podreze33@gmail.com>
2023-04-21 23:01:11 +0300

    Добавьте файлы проекта.

---
+++ TackManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TackManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Одна идея для проекта, использующего знания C# включает разработку приложения для управления списком задач (to-do list).
+
+            
+        }
+    }
+}
